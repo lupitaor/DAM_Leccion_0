f9371dc7e92eb8782c2fe0acca7e214b9888144d
Antonio Martinez Gonzalez <amartinez@itesa.edu.mx>
2025-01-28 18:52:52 -0600

    Versión_02: Instrucciones básicas en botón y caja de texto.

--- DAM_Leccion_AMG/MainPage.xaml.cs
+++ DAM_Leccion_AMG/MainPage.xaml.cs
@@ -1,4 +1,6 @@
-﻿namespace DAM_Leccion_AMG
+﻿using DAM_Leccion_AMG.Model;
+
+namespace DAM_Leccion_AMG
 {
     public partial class MainPage : ContentPage
     {
@@ -7,6 +9,7 @@
         public MainPage()
         {
             InitializeComponent();
+            Ejecutar();
         }
 
         private void OnCounterClicked(object sender, EventArgs e)
@@ -19,6 +22,21 @@
                 CounterBtn.Text = $"Clicked {count} times";
 
             SemanticScreenReader.Announce(CounterBtn.Text);
+        }
+
+        public void Ejecutar()
+        {
+            PersonaModel personaModel = new PersonaModel();
+
+            personaModel.Nombre = "Hola aqui estoy";
+
+            txtNombre.Text = personaModel.Nombre;
+        }
+
+        private void btnAceptar_Clicked(object sender, EventArgs e)
+        {
+            DisplayAlert("Asistente del sistema","Se ha guardado el registro en la BD","Aceptar");
+
         }
     }
 
---
+++ DAM_Leccion_AMG/Model/PersonaModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAM_Leccion_AMG.Model
+{
+    public class PersonaModel
+    {
+        public string Nombre { get; set; }
+
+    }
+}
